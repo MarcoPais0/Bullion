58a6956aaec85c7555244734c955b15a91b7e846
MarcoPais0 <marco.cruz.pais@gmail.com>
2022-07-22 15:11:51 +0100

    tests

--- App/exchange/tests.py
+++ App/exchange/tests.py
@@ -1,2 +1,19 @@
 from django.test import TestCase
 
+from users.models import User
+from .models import Balance, Favorite, Trade
+
+class BalanceTestCase(TestCase):
+
+    def setUp(self):
+        User.objects.create(username="Marco", password="Marco")
+        User.objects.create(username="Simao", password="Simao")
+        
+        Balance.objects.create(owner=User.objects.get(username="Marco"), amount=10, average_price=1, coin_id="usd", symbol="USD", name="Dolar", image="static/img/usd.png")
+        Balance.objects.create(owner=User.objects.get(username="Simao"), amount=30, average_price=1, coin_id="usd", symbol="USD", name="Dolar", image="static/img/usd.png")
+
+    def test_balance_correct(self):
+        marco = User.objects.get(username="Marco")
+        balance = Balance.objects.get(owner=marco, coin_id="usd")
+
+        self.assertEqual(balance.amount, 10)
--- App/users/tests.py
+++ App/users/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        User.objects.create(username="Marco", password="Marco")
+        User.objects.create(username="Simao", password="Simao")
+
+    def test_username_correct(self):
+        marco = User.objects.get(username="Marco")
+        simao = User.objects.get(username="Simao")
+
+        self.assertEqual("Marco", marco.username)
+        self.assertEqual("Simao", simao.username)
+
+    def test_password_correct(self):
+        marco = User.objects.get(username="Marco")
+        self.assertEqual(marco.password, "Marco")
+
+    def test_can_change_password(self):
+        marco = User.objects.get(username="Marco")
+        self.assertEqual(marco.password, "Marco")
+
+        marco.password = "pass"
+        self.assertEqual(marco.password, "pass")
+
+    def test_can_change_username(self):
+        marco = User.objects.get(username="Marco")
+        self.assertEqual(marco.username, "Marco")
+
+        marco.username = "pass"
+        self.assertEqual(marco.username, "pass")
+
